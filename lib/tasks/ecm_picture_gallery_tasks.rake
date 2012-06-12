0497f4587412d26be1088c36c8a3657c02c596a5
Roberto Vasquez Angel <robotex@robotex.de>
2012-06-12 17:56:43 +0200

    New build from scratch.

---
+++ lib/tasks/ecm_picture_gallery_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :ecm_picture_gallery do
+#   # Task goes here
+# end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'a56a819c847f20a6026d071403cc4f9c5a52b9cacc86476dc21a991581378bea704428de3c319e4b6ccc55dc90e0c7049bcabff36146bbd9b9eda32733477865'

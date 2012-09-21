75afaf245b475f24862711e750492a7045599247
Roberto Vasquez Angel <robotex@robotex.de>
2012-09-21 10:53:39 +0200

    Updated admin area.

--- lib/ecm/pictures/active_admin/ecm_pictures_picture_galleries.rb
+++ lib/ecm/pictures/active_admin/ecm_pictures_picture_galleries.rb
@@ -1,85 +1,86 @@
-include ActiveAdmin::ActsAsList::Helper
+include ActiveAdmin::ActsAsList::Helper if defined?(::ActiveAdmin)
 
-if defined?(ActiveAdmin)
-  ActiveAdmin.register Ecm::Pictures::PictureGallery do
-    menu :parent => I18n.t('ecm.pictures.active_admin.menu')
-    
-    config.sort_order = 'position'
-    sortable_member_actions
-    
-    form :html => { :enctype => "multipart/form-data" } do |f|
-      f.inputs do
-        f.input :name
-        f.input :description
-        f.input :link_images
-      end
-      
-     f.inputs do
-      f.has_many :pictures do |p|
-        p.input :name
-        #if !p.object.id.nil?
-        if p.object.persisted?
-          p.input :_destroy, :as => :boolean, :label => I18n.t('active_admin.delete')
-        end
-        p.input :image, :as => :file, :hint => p.template.image_tag(p.object.image.url(:thumb))
-        p.input :description
-      end
-    end      
-      
-      f.buttons
+ActiveAdmin.register Ecm::Pictures::PictureGallery do
+  menu :parent => I18n.t('ecm.pictures.active_admin.menu')
+
+  config.sort_order = 'position'
+  sortable_member_actions
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :link_images
     end
-    
-    index do
-      sortable_columns
-      column :name
-      column :pictures_count
-      column :link_images do |picture_gallery|
-        I18n.t(picture_gallery.link_images)
+
+   f.inputs do
+    f.has_many :pictures do |p|
+      p.input :name
+      if p.object.persisted?
+        p.input :_destroy, :as => :boolean, :label => I18n.t('active_admin.delete')
       end
-      column :created_at
-      column :updated_at
-      default_actions
-    end
-    
-    show do
-      attributes_table do
-        row :name
-        row :pictures_count
-        row :link_images do |picture_gallery|
-          I18n.t(picture_gallery.link_images)
-        end
-        row :created_at
-        row :updated_at
-      end
-      
-      panel Ecm::Pictures::PictureGallery.human_attribute_name(:description) do
-        div do
-          ecm_pictures_picture_gallery.description
-        end
-      end
-      
-      panel Ecm::Pictures::PictureGallery.human_attribute_name(:display_code) do
-        div do
-          ecm_pictures_picture_gallery.display_code
-        end
-      end      
-      
-      panel Ecm::Pictures::PictureGallery.human_attribute_name(:pictures) do
-        table_for ecm_pictures_picture_gallery.pictures, :i18n => Ecm::Pictures::Picture do
-          sortable_columns
-          column :thumbnail do |picture|
-            link_to(image_tag(picture.image.url(:thumb)), admin_ecm_pictures_picture_path(picture))
-          end
-          column :name
-          column :image_file_size, :sortable => :image_file_size do |picture|
-            number_to_human_size(picture.image_file_size)
-          end
-          column :created_at
-          column do |picture|
-            link_to I18n.t('active_admin.view'), admin_ecm_pictures_picture_path(picture), :class => "member_link view_link"
-          end
-        end
-      end
+      p.input :image, :as => :file, :hint => p.template.image_tag(p.object.image.url(:thumb))
+      p.input :description
     end
   end
-end  
+
+    f.buttons
+  end
+
+  index do
+    sortable_columns
+    column :name
+    column :pictures_count
+    column :link_images do |picture_gallery|
+      I18n.t(picture_gallery.link_images)
+    end
+    column :created_at
+    column :updated_at
+    default_actions
+  end
+
+  show do
+    panel Ecm::Pictures::PictureGallery.human_attribute_name(:description) do
+      div do
+        ecm_pictures_picture_gallery.description
+      end
+    end
+
+    panel Ecm::Pictures::PictureGallery.human_attribute_name(:display_code) do
+      div do
+        ecm_pictures_picture_gallery.display_code
+      end
+    end
+
+    panel Ecm::Pictures::PictureGallery.human_attribute_name(:pictures) do
+      table_for ecm_pictures_picture_gallery.pictures, :i18n => Ecm::Pictures::Picture do
+        sortable_columns
+        column :thumbnail do |picture|
+          link_to(image_tag(picture.image.url(:thumb)), admin_ecm_pictures_picture_path(picture))
+        end
+        column :name
+        column :image_file_size, :sortable => :image_file_size do |picture|
+          number_to_human_size(picture.image_file_size)
+        end
+        column :created_at
+        column do |picture|
+          link_to(I18n.t('active_admin.view'), [:admin, picture], :class => "member_link view_link") +
+          link_to(I18n.t('active_admin.edit'), [:edit, :admin, picture], :class => "member_link edit_link")
+        end
+      end
+    end
+  end # show
+
+  sidebar Ecm::Pictures::PictureGallery.human_attribute_name(:details), :only => :show do
+    attributes_table_for ecm_pictures_picture_gallery do
+      row :name
+      row :pictures_count
+      row :link_images do |picture_gallery|
+        I18n.t(picture_gallery.link_images)
+      end
+      row :created_at
+      row :updated_at
+    end
+  end # sidebar
+end if defined?(::ActiveAdmin)
+
--- lib/ecm/pictures/active_admin/ecm_pictures_pictures.rb
+++ lib/ecm/pictures/active_admin/ecm_pictures_pictures.rb
@@ -1,53 +1,61 @@
-include ActiveAdmin::ActsAsList::Helper
+include ActiveAdmin::ActsAsList::Helper if defined?(::ActiveAdmin)
 
-if defined?(ActiveAdmin)
-  ActiveAdmin.register Ecm::Pictures::Picture do
-    menu :parent => I18n.t('ecm.pictures.active_admin.menu')
-    
-    # active_admin-acts_as_list
-    config.sort_order = 'picture_gallery_id, position'
-    sortable_member_actions    
-    
-    form :html => { :enctype => "multipart/form-data" } do |f|
-      f.inputs do
-        f.input :picture_gallery
-        f.input :name
-        f.input :image, :as => :file, :hint => f.template.image_tag(f.object.image.url(:thumb))
-        f.input :description
+ActiveAdmin.register Ecm::Pictures::Picture do
+  menu :parent => I18n.t('ecm.pictures.active_admin.menu')
+
+  # active_admin-acts_as_list
+  config.sort_order = 'picture_gallery_id, position'
+  sortable_member_actions
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs do
+      f.input :picture_gallery
+      f.input :name
+      f.input :image, :as => :file, :hint => f.template.image_tag(f.object.image.url(:thumb))
+      f.input :description
+    end
+
+    f.buttons
+  end
+
+  index do
+    selectable_column
+    column :thumbnail do |picture|
+      link_to(image_tag(picture.image.url(:thumb)), admin_ecm_pictures_picture_path(picture))
+    end
+    column :picture_gallery
+    column :name
+    column :image_file_size, :sortable => :image_file_size do |picture|
+      number_to_human_size(picture.image_file_size)
+    end
+    column :created_at
+    default_actions
+  end
+
+  show do
+    panel Ecm::Pictures::Picture.human_attribute_name(:image) do
+      div  { image_tag ecm_pictures_picture.image.url }
+    end
+
+    panel Ecm::Pictures::Picture.human_attribute_name(:description) do
+      div do
+        ecm_pictures_picture.description
       end
-      
-      f.buttons
-    end    
-    
-    index do
-      column :thumbnail do |picture|
-        link_to(image_tag(picture.image.url(:thumb)), admin_ecm_pictures_picture_path(picture))
-      end
-      column :picture_gallery
-      column :name
-      column :image_file_size, :sortable => :image_file_size do |picture|
+    end
+  end
+
+  sidebar Ecm::Pictures::Picture.human_attribute_name(:details), :only => :show do
+    attributes_table_for ecm_pictures_picture do
+      row :picture_gallery
+      row :name
+      row :image_file_name
+      row :image_file_size do |picture|
         number_to_human_size(picture.image_file_size)
       end
-      column :created_at
-      default_actions
+      row :image_fingerprint
+      row :created_at
+      row :updated_at
     end
-    
-    show do
-      attributes_table do
-        row :picture_gallery
-        row :name
-        row :image_file_name
-        row :image_file_size do |picture|
-          number_to_human_size(picture.image_file_size)
-        end
-        row :image_fingerprint
-      end
-      
-      panel Ecm::Pictures::Picture.human_attribute_name(:description) do
-        div do
-          ecm_pictures_picture.description
-        end
-      end      
-    end
-  end 
-end  
+  end # sidebar
+end if defined?(::ActiveAdmin)
+

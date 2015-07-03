015066411eb3b294910d607d28fc5d6c58358027
Roberto Vasquez Angel <robotex@robotex.de>
2015-07-03 15:53:33 +0200

    Technical: Added all files from the 1.0.6.pre gem.

--- lib/ecm/pictures/configuration.rb
+++ lib/ecm/pictures/configuration.rb
@@ -9,34 +9,17 @@ module Ecm
         yield self
       end
 
-      mattr_accessor :markup_languages do
-        []
-      end
+      mattr_accessor :markup_languages
+      @@markup_languages = []
 
-      mattr_accessor :default_markup_language do
-        nil
-      end
+      mattr_accessor :default_markup_language
+      @@default_markup_language = nil
 
-      mattr_accessor :picture_css_classes do
-        'picture span3'
-      end
+      mattr_accessor :picture_for_gallery_css_classes
+      @@picture_for_gallery_css_classes = nil
 
-      mattr_accessor :picture_for_gallery_css_classes do
-        'picture span4'
-      end
-
-      mattr_accessor :picture_gallery_preview_div_css_classes do
-        'thumbnail'
-      end
-
-      mattr_accessor :picture_gallery_preview_li_css_classes do
-        'span4'
-      end
-
-      mattr_accessor :picture_image_styles do
-        {}
-      end
-
+      mattr_accessor :picture_image_styles
+      @@picture_image_styles = {}
       def picture_image_styles=(picture_image_styles)
         @@picture_image_styles = HashWithIndifferentAccess.new(picture_image_styles)
       end
--- lib/ecm_pictures.rb
+++ lib/ecm_pictures.rb
@@ -3,15 +3,16 @@ require 'acts_as_markup'
 require 'active_admin-acts_as_list'
 require 'friendly_id'
 require 'paperclip'
-require 'redcloth'
 
 require 'ecm/pictures/engine'
 require 'ecm/pictures/configuration'
 require 'ecm/pictures/routing'
 require 'ecm/pictures/version'
+require 'ecm/pictures/active_admin/pictureable_helper'
 
 module Ecm
   module Pictures
     extend Configuration
   end
 end
+
